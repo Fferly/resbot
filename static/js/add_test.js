b4f729420436d3de47f4c98e066f43d9c840960d
Andrii S <andrey.skorochod@gmail.com>
2023-06-07 00:47:54 +0300

    add question js logic working

---
+++ static/js/add_test.js
@@ -0,0 +1,49 @@
+'use strict'
+
+window.onload = () => {
+    const questionsContainer = document.getElementById('questions'),
+          addQuestionButton  = document.getElementById('add_question_button'); 
+
+    let currentQuestionNumber = 1;
+
+    const questionTemplate = `
+        <section class="question" id="question_{{current_number}} name=questions[]">
+            <div class="question__title">
+                Question {{current_number}}
+            </div>
+
+            <div class="options" id="options_{{current_number}}">
+                <div class="option">
+                    <input type="radio" name="question_{{current_number}}" value="">
+                    <input type="text"  name="text_{{current_number}}_1" id="">
+                </div>
+
+                <div class="option">
+                    <input type="radio" name="question_{{current_number}}" value="">
+                    <input type="text"  name="text_{{current_number}}_2" id="">
+                </div>  
+
+                <div class="option">
+                    <input type="radio" name="question_{{current_number}}" value="">
+                    <input type="text"  name="text_{{current_number}}_3" id="">
+                </div>  
+
+                <div class="option">
+                    <input type="radio" name="question_{{current_number}}" value="">
+                    <input type="text"  name="text_{{current_number}}_4" id="">
+                </div>  
+            </div>
+        </section>
+    `;
+
+
+    function addQuestion() {
+        let newQuestion = document.createElement('div'); 
+        newQuestion.innerHTML = questionTemplate.replaceAll('{{current_number}}', currentQuestionNumber);
+        console.log(questionTemplate.replace('{{current_number}}', currentQuestionNumber));
+        questionsContainer.appendChild(newQuestion);
+        currentQuestionNumber++;
+    };
+
+    addQuestionButton.addEventListener('mousedown', addQuestion);
+}
